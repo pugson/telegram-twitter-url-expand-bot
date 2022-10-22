6ecff77962310b59e66fa9f806617b3a06d1765c
Wojtek Witkowski <hi@wojtek.im>
2022-10-23 00:53:34 +0200

    Add support for Instagram links

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 import * as dotenv from "dotenv";
 import Tgfancy from "tgfancy";
-import fetch from "node-fetch";
 import { fetchTweet } from "./tweet-parser.js";
+import { trackEvent } from "./analytics.js";
 
 dotenv.config();
 
-const TWITTER_URL = /https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(es)?\/(\d+)/gim;
+const TWITTER_INSTAGRAM_URL =
+  /https?:\/\/(?:www\.)?(?:twitter\.com\/(?:#!\/)?(\w+)\/status(es)?\/(\d+)|instagram\.com\/(?:p|reel)\/([A-Za-z0-9-_]+))/gim;
 const bot = new Tgfancy(process.env.BOT_TOKEN, { polling: true });
 
-// 1. Match Twitter links
-bot.onText(TWITTER_URL, (msg, match, error) => {
-  // 2. Get the current Chat ID
+// Match Twitter or Instagram links
+bot.onText(TWITTER_INSTAGRAM_URL, (msg) => {
+  // Get the current Chat ID
   const chatId = msg.chat.id;
-
-  // 3. Get message text to parse links from
+  // Get message text to parse links from
   const msgText = msg.text;
+  // Iterate through all matched links in the message
+  msgText.match(TWITTER_INSTAGRAM_URL).forEach((link) => {
+    const isInstagram = link.includes("instagram.com");
+    const platform = isInstagram ? "Instagram post" : "Tweet";
 
-  // 4. Iterate through all links in the message
-  msgText.match(TWITTER_URL).forEach((link) => {
-    bot.sendMessage(chatId, "Expand this Tweet?", {
+    bot.sendMessage(chatId, `Expand this ${platform}?`, {
       reply_to_message_id: msg.message_id,
       reply_markup: {
         inline_keyboard: [
@@ -30,7 +32,7 @@ bot.onText(TWITTER_URL, (msg, match, error) => {
             },
             {
               text: "❌ No",
-              callback_data: `no`,
+              callback_data: isInstagram ? "no.instagram" : "no.twitter",
             },
           ],
         ],
@@ -39,27 +41,28 @@ bot.onText(TWITTER_URL, (msg, match, error) => {
   });
 });
 
-// 5. React to inline keyboard reply
+// React to inline keyboard reply
 bot.on("callback_query", async (answer) => {
   const chatId = answer.message.chat.id;
   const msgId = answer.message.message_id;
   const link = answer.data;
+  const isInstagram = link.includes("instagram");
 
-  if (link === "no") {
-    // 6a. Delete the bot reply so it doesn’t spam the chat
+  if (link.startsWith("no.")) {
+    // Delete the bot reply so it doesn’t spam the chat
     bot.deleteMessage(chatId, msgId);
-    fetch(`https://qckm.io?m=twitter.link.cancel&v=1&k=${process.env.QUICKMETRICS_TOKEN}`);
+    trackEvent(isInstagram ? "instagram.link.cancel" : "twitter.link.cancel");
     return;
   }
 
-  if (link === "undo") {
-    // 6b. Undo the link expansion and delete the bot reply
+  if (link.startsWith("undo.")) {
+    // Undo the link expansion and delete the bot reply
     bot.deleteMessage(chatId, msgId);
-    fetch(`https://qckm.io?m=twitter.link.undo&v=1&k=${process.env.QUICKMETRICS_TOKEN}`);
+    trackEvent(isInstagram ? "instagram.link.undo" : "twitter.link.undo");
     return;
   }
 
-  const expandTweet = (url) => {
+  const expandLink = (url) => {
     bot.editMessageText(url, {
       chat_id: chatId,
       message_id: msgId,
@@ -68,7 +71,7 @@ bot.on("callback_query", async (answer) => {
           [
             {
               text: "↩️ Undo",
-              callback_data: "undo",
+              callback_data: isInstagram ? "undo.instagram" : "undo.twitter",
               // callback_data has a 64 byte limit!!!
             },
           ],
@@ -76,22 +79,28 @@ bot.on("callback_query", async (answer) => {
       },
     });
 
-    fetch(`https://qckm.io?m=twitter.link.expand&v=1&k=${process.env.QUICKMETRICS_TOKEN}`);
+    trackEvent(isInstagram ? "instagram.link.expand" : "twitter.link.expand");
   };
 
-  // 7. Check if tweet has multiple images
-  fetchTweet(link)
-    .then((hasImages) => {
-      const replacement = hasImages ? "c.vxtwitter.com" : "vxtwitter.com";
-      const expandedLink = link.replace("twitter.com", replacement);
-      // 8a. Replace the reply with an expanded Tweet link
-      expandTweet(expandedLink);
-    })
-    .catch((error) => {
-      console.error(error);
-
-      // 8b. Fallback to vxtwitter.com if the API call fails
-      const expandedLink = link.replace("twitter.com", "vxtwitter.com");
-      expandTweet(expandedLink);
-    });
+  if (isInstagram) {
+    // Replace Instagram link
+    const newLink = link.replace("instagram.com", "ddinstagram.com");
+    expandLink(newLink);
+  } else {
+    // Check if tweet has multiple images
+    fetchTweet(link)
+      .then((hasImages) => {
+        const replacement = hasImages ? "c.vxtwitter.com" : "vxtwitter.com";
+        const newLink = link.replace("twitter.com", replacement);
+        // Replace the reply with an expanded Tweet link
+        expandLink(newLink);
+        if (hasImages) trackEvent("twitter.link.multipleImages");
+      })
+      .catch((error) => {
+        console.error(error);
+        // Fallback to vxtwitter.com if the API call fails
+        const newLink = link.replace("twitter.com", "vxtwitter.com");
+        expandLink(newLink);
+      });
+  }
 });
