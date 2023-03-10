005294c96de6d04d8ce49ff83bab87128a8c0fc7
pugson <hi@wojtek.im>
2023-03-10 03:17:43 +0100

    automatic and manual link expanding

--- callbacks/manual-expand.ts
+++ callbacks/manual-expand.ts
@@ -2,6 +2,7 @@ import { Context } from "grammy";
 import { trackEvent } from "../helpers/analytics";
 import { deleteMessage } from "../actions/delete-message";
 import { checkIfCached, deleteFromCache, getFromCache } from "../helpers/cache";
+import { expandLink } from "../actions/expand-link";
 
 /**
  * Handle Yes/No button responses to expand links
@@ -18,10 +19,10 @@ export async function handleManualExpand(ctx: Context) {
 
   if (data.includes("expand:no")) {
     const properties = data.split(":"); // expand:yes:chatId:messageId:linkIndex:platform
-    const originalChatId = properties[2];
-    const originalMessageId = properties[3];
-    const linkIndex = Number(properties[4]);
-    const platform = properties[5];
+    const originalChatId: string = properties[2];
+    const originalMessageId: string = properties[3];
+    const linkIndex: number = Number(properties[4]);
+    const platform: string = properties[5];
     const identifier = `${originalChatId}:${originalMessageId}:${linkIndex}`;
 
     await ctx.answerCallbackQuery();
@@ -37,43 +38,49 @@ export async function handleManualExpand(ctx: Context) {
 
   if (data.includes("expand:yes")) {
     const properties = data.split(":"); // expand:yes:chatId:messageId:linkIndex:platform:isDeletable
-    const originalChatId = properties[2];
-    const originalMessageId = properties[3];
-    const linkIndex = Number(properties[4]);
-    const platform = properties[5];
-    const isDeletable = properties[6] === "true";
+    const originalChatId: string = properties[2];
+    const originalMessageId: string = properties[3];
+    const linkIndex: number = Number(properties[4]);
+    const platform: string = properties[5];
+    const isDeletable: boolean = properties[6] === "true";
     const identifier = `${originalChatId}:${originalMessageId}:${linkIndex}`;
     const prevLinkIdentifier = `${originalChatId}:${originalMessageId}:${linkIndex - 1}`;
     const nextLinkIdentifier = `${originalChatId}:${originalMessageId}:${linkIndex + 1}`;
     const hasPrevLink: boolean = await checkIfCached(prevLinkIdentifier);
     const hasNextLink: boolean = await checkIfCached(nextLinkIdentifier);
-    const messageFromCache: any = await getFromCache(identifier);
+    const contextFromCache: any = await getFromCache(identifier);
+    const cachedMessage = contextFromCache?.update?.message;
+    const urlOffset: number = cachedMessage?.entities?.[linkIndex].offset;
+    const urlLength: number = cachedMessage?.entities?.[linkIndex].length;
+    const url: string = cachedMessage?.text.slice(urlOffset, urlOffset + urlLength);
+    const entities = contextFromCache.entities();
+    const message = cachedMessage?.text ?? cachedMessage?.caption ?? "";
+    const messageWithNoLinks = entities.reduce(
+      (msg: string, entity: { text: any }) => msg.replace(entity.text, ""),
+      message
+    );
 
     // Only expand when a message has been cached, otherwise ignore the callback
     // because it will throw an error when trying to delete the message.
-    if (messageFromCache) {
-      deleteMessage(chatId, messageId); // bot’s message with buttons
+    if (contextFromCache) {
+      deleteMessage(chatId, messageId); // bot’s [yes][no] message
+
+      const userInfo = {
+        username: cachedMessage.from?.username,
+        firstName: cachedMessage.from?.first_name,
+        lastName: cachedMessage.from?.last_name,
+        userId: cachedMessage.from?.id,
+      };
+
+      await expandLink(ctx, url, messageWithNoLinks, userInfo);
+
+      trackEvent(`expand.yes.${platform}`);
 
       // When multiple links are in the message the bot will send a reply for each link.
       // Delete the original message only if it's the last link in the message.
       if (!hasPrevLink && !hasNextLink) {
-        try {
-          // Gotta await try/catch this because the original message might have been deleted already
-          // and the bot will crash if it tries to delete a message that does not exist.
-          if (isDeletable) await deleteMessage(originalChatId, Number(originalMessageId));
-        } catch (error) {
-          console.error(error);
-        }
+        if (isDeletable) await deleteMessage(originalChatId, Number(originalMessageId), ctx);
       }
-
-      // TODO: temp reply
-      // ctx.reply(messageFromCache.update.message.text); // reduce to only include the right link index
-
-      //
-      // expand-link.ts here with all needed params
-      //
-
-      trackEvent(`expand.yes.${platform}`);
     }
 
     await ctx.answerCallbackQuery();
