f24c3a901e6d55102fd565f51d50a6e5df8d5459
pugson <hi@wojtek.im>
2023-03-10 04:50:58 +0100

    tweet parser with cache

--- .env-typings.d.ts
+++ .env-typings.d.ts
@@ -2,7 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     TELEGRAM_BOT_TOKEN: string;
     ADMIN_TELEGRAM_ID: string;
-    TWITTER_TOKEN: string;
+    PASSKEY: string;
     XATA_API_KEY?: string;
     XATA_FALLBACK_BRANCH?: string;
   }
--- helpers/tweet-parser.ts
+++ helpers/tweet-parser.ts
@@ -1,19 +1,11 @@
 import axios from "axios";
 
-// TODO: Add an edge cache to avoid hitting the API too much and improve performance.
-// Deploy an endpoint that will fetch the tweet and cache the result for 7 days.
 export const fetchTweet = async (url: string): Promise<boolean> => {
   try {
+    // @ts-expect-error Object is possibly 'undefined'.ts(2532)
     const tweetId = url.split("/").pop().split("?")[0];
-    const response = await axios.get(
-      `https://api.twitter.com/1.1/statuses/show.json?id=${tweetId}&tweet_mode=extended`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.TWITTER_TOKEN}`,
-        },
-      }
-    );
-    const hasImages = response.data.extended_entities && response.data.extended_entities.media.length > 0;
+    const response = await axios.get(`https://tweet-api-cache.vercel.app/${tweetId}?passkey=${process.env.PASSKEY}`);
+    const hasImages = response.data.extended_entities && response.data.extended_entities.media.length > 1;
     return hasImages;
   } catch (error) {
     console.error(url, error);
