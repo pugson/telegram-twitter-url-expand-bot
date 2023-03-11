eb0f4bb5c39bd9078b7b79ae2e93486fcc078b51
pugson <hi@wojtek.im>
2023-03-11 04:20:16 +0100

    improve tweet parse error handling

--- helpers/tweet-parser.ts
+++ helpers/tweet-parser.ts
@@ -1,14 +1,20 @@
 import axios from "axios";
+import { notifyAdmin } from "./notifier";
 
 export const fetchTweet = async (url: string): Promise<boolean> => {
+  let hasImages = false;
+
   try {
     // @ts-expect-error Object is possibly 'undefined'.ts(2532)
     const tweetId = url.split("/").pop().split("?")[0];
     const response = await axios.get(`https://tweet-api-cache.vercel.app/${tweetId}?passkey=${process.env.PASSKEY}`);
-    const hasImages = response.data.extended_entities && response.data.extended_entities.media.length > 1;
-    return hasImages;
+    hasImages = response.data.extended_entities && response.data.extended_entities.media.length > 1;
   } catch (error) {
-    console.error(url, error);
-    return false;
+    // @ts-ignore
+    console.error(url, error.message);
+    // @ts-ignore
+    notifyAdmin(`url: ${url}, message: ${error.message}`);
   }
+
+  return hasImages;
 };
