52a13c05e734027d1c44821695a7a302b039bdb9
Wojtek Witkowski <hi@wojtek.im>
2024-01-20 19:49:42 +0000

    right back at it again

--- helpers/banned.ts
+++ helpers/banned.ts
@@ -1,4 +1,4 @@
 // List of malicious chats that were trying to crash the bot.
-const banList: number[] = [1947938299, -1001226058268, -1002124315683];
+const banList: number[] = [1947938299, -1001226058268, -1002124315683, -1001149568794];
 
 export const isBanned = (chatId: number) => banList.includes(Number(chatId));
