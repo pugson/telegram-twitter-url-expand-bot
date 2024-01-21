cde69ed2e81d529b90e8abbb226c7feda4ea9bd3
pugson <hi@wojtek.im>
2024-01-21 11:45:38 +0000

    another one

--- helpers/banned.ts
+++ helpers/banned.ts
@@ -1,4 +1,10 @@
 // List of malicious chats that were trying to crash the bot.
-const banList: number[] = [1947938299, -1001226058268, -1002124315683, -1001149568794, -1001493829609];
+const banList: number[] = [
+  1947938299, 
+  -1001226058268, 
+  -1002124315683, 
+  -1001149568794, 
+  -1001493829609,
+  6908519864];
 
 export const isBanned = (chatId: number) => banList.includes(Number(chatId));
