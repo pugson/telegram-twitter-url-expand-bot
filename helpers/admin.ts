f1ee3c1a3dc7dceacf543e5e1299a9187458317b
Wojtek Witkowski <hi@wojtek.im>
2024-02-21 20:38:46 +0100

    hotfix chat member — Bad Request: user not found

--- helpers/admin.ts
+++ helpers/admin.ts
@@ -3,27 +3,32 @@
 import { Context } from "grammy";
 
 export async function checkAdminStatus(ctx: Context) {
-  // No chat = no service
-  if (!ctx.chat) {
+  try {
+    // No chat = no service
+    if (!ctx.chat) {
+      return false;
+    }
+    // Channels and private chats are only postable by admins
+    if (["channel", "private"].includes(ctx.chat.type)) {
+      return true;
+    }
+    // Anonymous users are always admins
+    if (ctx.from && ctx.from.username === "GroupAnonymousBot") {
+      return true;
+    }
+    // Surely not an admin
+    if (!ctx.from || !ctx.from.id) {
+      return false;
+    }
+    // Check the member status
+    const chatMember = await ctx.getChatMember(ctx.from.id);
+    if (["creator", "administrator"].includes(chatMember.status)) {
+      return true;
+    }
+    // Not an admin by default
+    return false;
+  } catch (e) {
+    console.error(e);
     return false;
   }
-  // Channels and private chats are only postable by admins
-  if (["channel", "private"].includes(ctx.chat.type)) {
-    return true;
-  }
-  // Anonymous users are always admins
-  if (ctx.from && ctx.from.username === "GroupAnonymousBot") {
-    return true;
-  }
-  // Surely not an admin
-  if (!ctx.from || !ctx.from.id) {
-    return false;
-  }
-  // Check the member status
-  const chatMember = await ctx.getChatMember(ctx.from.id);
-  if (["creator", "administrator"].includes(chatMember.status)) {
-    return true;
-  }
-  // Not an admin by default
-  return false;
 }
