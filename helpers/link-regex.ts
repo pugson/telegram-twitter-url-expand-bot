1b6bb6f07c4a51236e7d7a9ecc84572b745ac32f
Rafa M <rafitamolin@users.noreply.github.com>
2024-04-27 15:15:32 +0100

    Add support for Instagram stories (#58)
    * Add support for Instagram stories
    
    * Fix twitter.com example showing in instagram.com, add URL structure for Instagram stories
    
    ---------
    
    Co-authored-by: pugson <hi@wojtek.im>

--- helpers/link-regex.ts
+++ helpers/link-regex.ts
@@ -8,8 +8,8 @@
     - https://www.twitter.com/username/statuses/status_id
     
  * Instagram post URLs, in the format:
-    - https://twitter.com/username/statuses/status_id
     - https://mobile.instagram.com/p/post_id
+    - https://www.instagram.com/stories/username/post_id
     - https://www.instagram.com/p/post_id
     - https://www.instagram.com/reel/post_id
     - https://instagram.com/p/post_id
@@ -33,4 +33,4 @@
     - https://news.ycombinator.com/item?id=post_id
  */
 export const LINK_REGEX: RegExp =
-  /https?:\/\/(?:www\.)?(?:mobile\.)?(?:(?:twitter|x)\.com\/(?:#!\/)?(\w+)\/status(es)?\/(\d+)(?:\?.*)?|instagram\.com\/(?:p|reel)\/([A-Za-z0-9-_]+)(?:\?.*)?|(?:lite\.|www\.|)?tiktok\.com\/(?:@|v\/)?(\w+)\/(video\/)?(\d+)(?:\?.*)?|(?:vm\.|id\.|en\.|lite\.)tiktok\.com\/([A-Za-z0-9-_]+)(?:\?.*)?|www\.tiktok\.com\/(@[\w.-]+\/video\/\d+|v\/\d+|t\/\w+)(?:\?.*)?|posts\.cv\/([A-Za-z0-9_]+)\/([A-Za-z0-9]+)(?:\?.*)?|news\.ycombinator\.com\/item\?id=\d+(?:\?.*)?)/im;
+  /https?:\/\/(?:www\.)?(?:mobile\.)?(?:(?:twitter|x)\.com\/(?:#!\/)?(\w+)\/status(es)?\/(\d+)(?:\?.*)?|instagram\.com\/(?:p|reel|stories\/[^\/]+)\/([A-Za-z0-9-_]+)(?:\?.*)?|(?:lite\.|www\.|)?tiktok\.com\/(?:@|v\/)?(\w+)\/(video\/)?(\d+)(?:\?.*)?|(?:vm\.|id\.|en\.|lite\.)tiktok\.com\/([A-Za-z0-9-_]+)(?:\?.*)?|www\.tiktok\.com\/(@[\w.-]+\/video\/\d+|v\/\d+|t\/\w+)(?:\?.*)?|posts\.cv\/([A-Za-z0-9_]+)\/([A-Za-z0-9]+)(?:\?.*)?|news\.ycombinator\.com\/item\?id=\d+(?:\?.*)?)/im;
