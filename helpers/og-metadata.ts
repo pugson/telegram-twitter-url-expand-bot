338a0ba2bdcca28e6f356295cff7edb5ba233c94
pugson <hi@wojtek.im>
2024-11-02 03:50:03 +0100

    extend Spotify scraper timeout

--- helpers/og-metadata.ts
+++ helpers/og-metadata.ts
@@ -5,7 +5,7 @@ export const getOGMetadata = async (link: string) => {
 
   try {
     const response = await axios.get(`https://og.metadata.vision/${link}`, {
-      timeout: 15_000,
+      timeout: 60_000,
     });
     return response.data.data;
   } catch (error) {
