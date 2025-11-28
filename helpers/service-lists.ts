9fd32c81b332bd0a7c552434ac8fdecf088a758e
Rafa M. <rafitamolin@users.noreply.github.com>
2025-11-28 12:17:56 +0100

    Create service-lists.ts with social media domains
    Added lists of domains for Instagram, TikTok, and Twitter.

---
+++ helpers/service-lists.ts
@@ -0,0 +1,20 @@
+export const INSTAGRAM_DOMAINS = [
+  "zzinstagram.com",
+  "fxstagram.com",
+  "eeinstagram.com",
+  "uuinstagram.com",
+  "vxinstagram.com",
+  "kkinstagram.com",
+];
+
+export const TIKTOK_DOMAINS = [
+  "tfxktok.com",
+  "kktiktok.com",
+  "tnktok.com",
+  "tiktxk.com",
+];
+
+export const TWITTER_DOMAINS = [
+  "fixupx.com",
+  "fixvx.com",
+];
