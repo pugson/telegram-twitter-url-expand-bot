561f762b3c3544b4f3f5251cf405c3e7abd788e8
Rafa M. <rafitamolin@users.noreply.github.com>
2025-11-28 12:20:35 +0100

    Add handleSwitchService to callback query handler

--- callbacks/index.ts
+++ callbacks/index.ts
@@ -8,12 +8,9 @@ import { handleLockSettings } from "./settings-lock";
 import { handleChangelogSettings } from "./settings-changelog";
 import { handlePermissionsSettings } from "./settings-permissions";
 import { handleUndo } from "./undo";
+import { handleSwitchService } from "./switch-service";
 import { isBanned } from "../helpers/banned";
 
-// Multiple bot.on("callback_query") functions cannot run in parallel.
-// The bot will only register the first one and ignore the rest.
-// This file runs the bot.on("callback_query") listener and imports functions
-// that are specific to the callback_query data to keep it more clean.
 bot.on("callback_query", async (ctx: Context) => {
   const chatId = ctx.update?.callback_query?.message?.chat.id;
   const data = ctx.update?.callback_query?.data;
@@ -21,16 +18,15 @@ bot.on("callback_query", async (ctx: Context) => {
   if (!chatId || !data) return;
   if (isBanned(chatId)) return;
 
-  // Handle expand callbacks
   await handleManualExpand(ctx);
   await handleExpandedLinkDestruction(ctx);
   await handleUndo(ctx);
+  await handleSwitchService(ctx);
   await handleAutoexpandSettings(ctx);
   await handleLockSettings(ctx);
   await handleChangelogSettings(ctx);
   await handlePermissionsSettings(ctx);
 
-  // Save anonymous chat member count to database
   getMemberCount(chatId).catch(() => {
     console.warn(`[Warning] Could not get member count for chat ${chatId}`);
   });
